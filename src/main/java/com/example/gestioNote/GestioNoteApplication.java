76785191311cadd37f59627e133e4e47ce0fe655
idealyy <idealytsitohaina@gmail.com>
2025-05-03 12:58:46 +0300

    initial commit

---
+++ src/main/java/com/example/gestioNote/GestioNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestioNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestioNote/GestioNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestioNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
